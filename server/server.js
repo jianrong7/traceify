c71c5a6b160c9e3e9f21e5a62dcf2bf0e9d7b7db
Loh Jian Rong <ipwnjianrong@gmail.com>
2021-09-01 11:21:55 +0800

    First commit

---
+++ server/server.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/refresh', (req, res) => {
+  const refreshToken = req.body.refreshToken
+  const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENTID,
+    clientSecret: process.env.CLIENTSECRET,
+    redirectUri: process.env.REDIRECTURI,
+    refreshToken
+  })
+
+  spotifyApi.refreshAccessToken().then(data => {
+    res.json({
+      accessToken: data.body.access_token,
+      expiresIn: data.body.expires_in
+    })
+  }).catch(() => {
+    res.sendStatus(400)
+  })
+})
+
+app.post('/login', (req, res) => {
+  const code = req.body.code
+  const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENTID,
+    clientSecret: process.env.CLIENTSECRET,
+    redirectUri: process.env.REDIRECTURI,
+  })
+
+  spotifyApi.authorizationCodeGrant(code).then(data => {
+    res.json({
+      accessToken: data.body.access_token,
+      refreshToken: data.body.refresh_token,
+      expiresIn: data.body.expires_in
+    })
+  }).catch(() => {
+    res.sendStatus(400)
+  })
+})
+
+app.listen(3001)
